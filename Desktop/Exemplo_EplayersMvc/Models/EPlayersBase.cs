c085899c034d244ee31fb5b47aaa745e54ba659b
nickrci <nikrci@hotmail.com>
2021-12-22 22:47:09 -0300

    Consumir

---
+++ Desktop/Exemplo_EplayersMvc/Models/EPlayersBase.cs
@@ -0,0 +1,17 @@
+
+namespace Exemplo_EplayersMvc.Models
+{
+    public class EPlayersBase
+    {
+        public void CriarPastaEArquivo(string _caminho){
+
+            string pasta = _caminho.Split("/")[0];
+            string arquivo = _caminho.Split("/")[1];
+
+            if (Directory.Exists())
+            {
+                
+            }
+        }
+    }
+}
